cfc222d5887bdd42dd1ceac83031ede8de10b7a4
Mehmet Dolasan <mehmetdolasan@gmail.com>
2022-04-18 00:59:09 +0300

    Initial Commit

---
+++ Present Modally Kod ile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Present Modally Kod ile
+//
+//  Created by Mehmet Dolasan on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
