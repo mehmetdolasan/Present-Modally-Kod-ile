4e12cac1c74834680d596d6a9da379f3d9b35208
Mehmet Dolasan <mehmetdolasan@gmail.com>
2022-04-18 01:45:26 +0300

    finished the segues

--- Present Modally Kod ile/ViewController2.swift
+++ Present Modally Kod ile/ViewController2.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewController2.swift
 //  Present Modally Kod ile
 //
 //  Created by Mehmet Dolasan on 18.04.2022.
@@ -7,18 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController2: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-    @IBAction func goto2(_ sender: Any) {
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func goto3(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let gidilecekVC = storyboard.instantiateViewController(withIdentifier: "sayfa2") as! ViewController2
+        let gidilecekVC = storyboard.instantiateViewController(withIdentifier: "sayfa3") as! ViewController3
         self.present(gidilecekVC, animated: true, completion: nil)
     }
     
+    @IBAction func geriDismiss(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
-
--- Present Modally Kod ile/ViewController3.swift
+++ Present Modally Kod ile/ViewController3.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewController3.swift
 //  Present Modally Kod ile
 //
 //  Created by Mehmet Dolasan on 18.04.2022.
@@ -7,18 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController3: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func goto1(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let gidilecekVC = storyboard.instantiateViewController(withIdentifier: "sayfa1") as! ViewController
+        self.present(gidilecekVC, animated: true, completion: nil)
+    }
     @IBAction func goto2(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let gidilecekVC = storyboard.instantiateViewController(withIdentifier: "sayfa2") as! ViewController2
         self.present(gidilecekVC, animated: true, completion: nil)
     }
     
-}
 
+}
